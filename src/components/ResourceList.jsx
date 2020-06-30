7a30804841a026c64a12204ad1bb602a933b4890
unknown <wchamberlain89@gmail.com>
2020-06-29 20:57:30 -0700

    Resources will now be organized by resource type.
    Resource List Component will list a users settlment resources
    by resource type including headers.
    
    When adding a new resource users will select a resource type from a
    dropdown filtering available resources to that resource type.

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -3,26 +3,36 @@ import PropTypes from 'prop-types';
 import Counter from './Counter';
 import ResourceListItem from './ResourceListItem';
 import groupBy from '../utils/groupBy';
+import _ from 'lodash';
 
 const ResourceList = (props) => {
   const { settlementId, resources } = props;
 
-  console.log(groupBy(resources, 'qty'));
+  const groupedResources = _.groupBy(resources, 'resourceInfo.resourceType');
+  
+  console.log(groupedResources);
 
   return (
     <>
-      <ul>
-        {resources.map(( resource ) => {
-          return <ResourceListItem 
-            key={resource.resourceId} 
-            name={resource.resourceInfo.name}
-            qty={resource.qty} 
-            resourceId={resource.resourceId}
-            settlementId={settlementId}
-            onClick={() => props.onClick(resource)}
-          />
-        })}
-      </ul>
+        {
+          Object.keys(groupedResources).map( resourceType => (
+            <>
+              <strong><h3 style={{marginTop: '2em'}}>{resourceType}</h3></strong>
+              {
+                groupedResources[resourceType].map( resource => (
+                  <ResourceListItem 
+                    key={resource.resourceId} 
+                    name={resource.resourceInfo.name}
+                    qty={resource.qty} 
+                    resourceId={resource.resourceId}
+                    settlementId={settlementId}
+                    onClick={() => props.onClick(resource)}
+                  />
+                ))
+              }
+            </>
+          ))
+        }
     </>
   )
 }
--- src/components/ResourceSelect.jsx
+++ src/components/ResourceSelect.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 
 const ResourceSelect = ({ register }) => {
   const [resources, setResources] = useState([]);
-
+  const [selectedResourceType, setSelectedResourceType] = useState(null);
+  
+  const groupedResources = _.groupBy(resources, "resourceType");
+  console.log(groupedResources);
+  console.log(groupedResources.selectedResourceType, "selected resource type");
+  const handleSelectFieldChange = (event) => {
+    event.preventDefault();
+    setSelectedResourceType(event.target.value);
+  }
+  
   useEffect(() => {
     fetch("http://localhost:7000/assets/resources")
     .then(res => res.json())
@@ -14,13 +24,21 @@ const ResourceSelect = ({ register }) => {
   }, []);
 
   return (
-    <select name="resource" id="resourceId" ref={register}>
-      {resources.map((resource) => {
-        return (
-          <option value={resource.resourceId}>{resource.name}</option>
-        )
-      })}
-    </select>
+    <>
+      <select name="resourceType" id="resourcetype" onChange={event => handleSelectFieldChange(event)}>
+        <option value={null}>Select Resource Type</option>
+        {Object.keys(groupedResources).map((resourceType) => (
+          <option value={resourceType}>{resourceType}</option>
+        ))}
+      </select>
+      
+      <select name="resource" id="resourceId" ref={register}>
+         {!selectedResourceType ? <option disabled>Select Resource Type</option> :
+          selectedResourceType && groupedResources[selectedResourceType].map((resource) => (
+            <option value={resource.resourceId}>{resource.name}</option>
+          ))}
+      </select>
+    </>
   );
 };
 
